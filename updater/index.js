efc1ab1da29c191740609bc09787e0416baf9bb0
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2015-10-27 17:17:54 -0200

    Fixing js

--- updater/index.js
+++ updater/index.js
@@ -8,9 +8,9 @@
 		io.sockets.on("connection", function(socket){
 			console.log("Socket was connected");
 
-			socket.on("join category", function(category{
+			socket.on("join category", function(category){
 				socket.join(category);
-			}));
+			});
 
 			socket.on("newNote", function(data){
 				socket.broadcast.to(data.category).emit("broadcast note", data.note);
